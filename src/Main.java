1ab52147ead164ad6f6b74dd2b165ee477f5add1
Michela-DC <michela.delconte.b@gmail.com>
2023-06-09 12:40:06 +0200

    esercizio String-handling

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+// Define a string called string1 that has assigned the value "Hello"
+// Define a string called string2 that has assigned the value "How are you?"
+// Take the first 2 characters string1 and the last 2 characters of string2 and combine the 4 chars (Heu?) into a dedicated array called charArray
+// Print charArray
+
+public class Main {
+    public static void main(String[] args) {
+       String string1 = "Hello";
+       String string2 = "How are you?";
+
+       String combinedString = string1.substring(0,2) + string2.substring(10,12);
+
+       char [] charArray = combinedString.toCharArray();
+
+       System.out.println(charArray);
+    }
+}
